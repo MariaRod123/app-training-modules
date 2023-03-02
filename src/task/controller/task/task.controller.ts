13925dbb50e9b9d972e42171e525aacf92a12479
María <72228855+MariaRod123@users.noreply.github.com>
2023-03-02 14:26:14 -0300

    Update task.controller.ts

--- src/task/controller/task/task.controller.ts
+++ src/task/controller/task/task.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { TaskService } from 'src/task/service/task/task.service';
 
-@Controller('task/message')
+@Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
-  @Get()
+  @Get('message')
   getTask(): string {
     return this.taskService.getTask();
   }
