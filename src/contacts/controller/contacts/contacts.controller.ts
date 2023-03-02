6b8d14dd212693fd977bff211e67d865e3bf320a
María <72228855+MariaRod123@users.noreply.github.com>
2023-03-02 14:25:26 -0300

    Update contacts.controller.ts

--- src/contacts/controller/contacts/contacts.controller.ts
+++ src/contacts/controller/contacts/contacts.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ContactsService } from 'src/contacts/service/contacts/contacts.service';
 
-@Controller('contacts/message')
+@Controller('contacts')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
 
-  @Get()
+  @Get('message')
   getContacts(): string {
     return this.contactsService.getContacts();
   }
